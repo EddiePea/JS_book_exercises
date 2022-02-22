96e9481a0c1924ded1d9154bf906312516ca56b0
Eddie Pea <edmapearce@gmail.com>
2022-02-22 16:41:08 +0000

    Exercises continued

--- rlSync_practice.js
+++ rlSync_practice.js
@@ -6,7 +6,8 @@ let city = rlSync.question('Where do you live?\n');
 let currentYear = new Date().getFullYear();
 
 console.log(`My name is ${name} and I am ${age} years old. 
-I was born in around ${currentYear - age} and I live in ${city}.`);
+I was born in around ${currentYear - age} (plus or minus a year) 
+and I live in ${city}.`);
 
 /*This doesn't quite work as you need the current month and the birth 
 month to calculate birth year exactly*/
