825edc76fd91fee1d47a9bbc1c045ff2fb3a6bfd
Eddie Pea <edmapearce@gmail.com>
2022-02-22 16:39:27 +0000

    Exercises continued

---
+++ rlSync_practice.js
@@ -0,0 +1,12 @@
+let rlSync = require('readline-sync');
+
+let name = rlSync.question('What is your name?\n');
+let age = Number(rlSync.question('Enter your age\n'));
+let city = rlSync.question('Where do you live?\n');
+let currentYear = new Date().getFullYear();
+
+console.log(`My name is ${name} and I am ${age} years old. 
+I was born in around ${currentYear - age} and I live in ${city}.`);
+
+/*This doesn't quite work as you need the current month and the birth 
+month to calculate birth year exactly*/
