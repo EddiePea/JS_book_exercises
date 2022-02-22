3252d75ebd88e2b751a28147dde148a82898bc37
Eddie Pea <edmapearce@gmail.com>
2022-02-22 15:23:35 +0000

    Exercises

---
+++ age.js
@@ -0,0 +1,7 @@
+let age = 20;
+let timePast = 10;
+
+console.log(`In ${timePast} years, you will be ${age + timePast} years old.`);
+console.log(`In ${timePast * 2} years, you will be ${age + (timePast * 2)} years old.`);
+console.log(`In ${timePast * 3} years, you will be ${age + (timePast * 3)} years old.`);
+console.log(`In ${timePast * 4} years, you will be ${age + (timePast * 4)} years old.`);
---
+++ basics.js
@@ -0,0 +1,9 @@
+//Operators 
+
+console.log(42 === 42);
+
+console.log(42 === 43);
+
+'foo' === 'foo';
+
+'FOO' === 'foo';
