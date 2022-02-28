eec6291843d3a30c3fe1a70febfaec48edf9f234
Eddie Pea <edmapearce@gmail.com>
2022-02-28 19:17:46 +0000

    Function exercises

---
+++ greeting.js
@@ -0,0 +1,12 @@
+function greetPeople(greetingMessage) {
+  console.log(greetingMessage);
+}
+
+greetPeople("Good Morning!");
+
+
+greetPersons();
+
+function greetPersons() {
+  console.log("Hiya");
+}
