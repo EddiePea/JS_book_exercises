4741376d1c4ad75b2486dad372ff0091eca11124
Eddie Pea <edmapearce@gmail.com>
2022-03-01 12:09:19 +0000

    Conditionals execises

---
+++ switch.js
@@ -0,0 +1,34 @@
+let a = 8;
+
+switch (a) {
+  case 5: 
+    console.log('a is 5');
+    break;
+  case 6: 
+    console.log('a is 6');
+    break;
+  default: 
+    console.log('a is neither 5, nor 6');
+    break;
+}
+
+
+let b = 5;
+
+if (b === 5) {
+  console.log('b is 5');
+} else if (b === 6) {
+  console.log('b is 6');
+} else {
+  console.log('b is neither 5, nor 6');
+}
+
+let c = 10;
+
+if (c === 5) {
+  console.log('c is 5');
+} else if (c === 6) {
+  console.log('c is 6');
+} else {
+  console.log('c is something else!');
+}
