a13312663dff16a789fe2448cdf679e846076ae5
Eddie Pea <edmapearce@gmail.com>
2022-03-01 11:03:04 +0000

    Functions exercises

--- greeter.js
+++ greeter.js
@@ -1,5 +1,17 @@
-let name = 'Victor';
 
-console.log(`Good Morning, ${name}.`);
-console.log(`Good Afternoon, ${name}.`);
-console.log(`Good Evening, ${name}.`);
+let getFirstName = (text) => {
+  let input = prompt(text);
+  return input;
+}
+
+let getSecondName = (text) => {
+  let input = prompt(text);
+  return input; 
+}
+
+let firstName = getFirstName("Enter your first name");
+let secondName = getSecondName("Enter your second name");
+
+console.log(`Good Morning, ${firstName} ${secondName}.`);
+console.log(`Good Afternoon, ${firstName} ${secondName}.`);
+console.log(`Good Evening, ${firstName} ${secondName}.`);
